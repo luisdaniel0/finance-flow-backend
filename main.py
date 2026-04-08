3f44cfb1ee5638d8144c7157b8c3541c62962c93
luisdaniel0 <luisanchezdh@gmail.com>
2026-04-07 20:29:57 -0400

    added CORS, foreign keys, user-filtered routes, JWT auth--date=04/06/2026

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
@@ -85,6 +86,14 @@ class Token(BaseModel):
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
@@ -94,7 +103,12 @@ async def root():
 @app.get("/transactions/")
 async def get_transactions(db=Depends(get_db), current_user=Depends(get_current_user)):
 
-    transactions = db.query(TransactionModel).filter(TransactionModel.user_id == current_user.id).order_by(TransactionModel.id).all()
+    transactions = (
+        db.query(TransactionModel)
+        .filter(TransactionModel.user_id == current_user.id)
+        .order_by(TransactionModel.id)
+        .all()
+    )
     return transactions
 
 
@@ -121,7 +135,12 @@ async def get_single_transaction(
     transaction_id: int, db=Depends(get_db), current_user=Depends(get_current_user)
 ):
     transaction = (
-        db.query(TransactionModel).filter(TransactionModel.id == transaction_id, TransactionModel.user_id == current_user.id).first()
+        db.query(TransactionModel)
+        .filter(
+            TransactionModel.id == transaction_id,
+            TransactionModel.user_id == current_user.id,
+        )
+        .first()
     )
     if transaction is None:
         raise HTTPException(status_code=404, detail="Transaction Not Found")
@@ -136,7 +155,12 @@ async def edit_transaction(
     current_user=Depends(get_current_user),
 ):
     edit_transaction = (
-        db.query(TransactionModel).filter(TransactionModel.id == transaction_id, TransactionModel.user_id == current_user.id).first()
+        db.query(TransactionModel)
+        .filter(
+            TransactionModel.id == transaction_id,
+            TransactionModel.user_id == current_user.id,
+        )
+        .first()
     )
 
     if edit_transaction is None:
@@ -157,7 +181,12 @@ async def delete_transaction(
     transaction_id: int, db=Depends(get_db), current_user=Depends(get_current_user)
 ):
     transaction = (
-        db.query(TransactionModel).filter(TransactionModel.id == transaction_id, TransactionModel.user_id == current_user.id).first()
+        db.query(TransactionModel)
+        .filter(
+            TransactionModel.id == transaction_id,
+            TransactionModel.user_id == current_user.id,
+        )
+        .first()
     )
     if transaction is None:
         raise HTTPException(status_code=404, detail="Transaction Not Found")
@@ -168,7 +197,12 @@ async def delete_transaction(
 
 @app.get("/budgets/")
 async def get_budgets(db=Depends(get_db), current_user=Depends(get_current_user)):
-    budgets = db.query(BudgetModel).filter(BudgetModel.user_id == current_user.id).order_by(BudgetModel.id).all()
+    budgets = (
+        db.query(BudgetModel)
+        .filter(BudgetModel.user_id == current_user.id)
+        .order_by(BudgetModel.id)
+        .all()
+    )
     return budgets
 
 
@@ -177,7 +211,10 @@ async def create_budgets(
     budget: Budget, db=Depends(get_db), current_user=Depends(get_current_user)
 ):
     new_budget = BudgetModel(
-        name=budget.name, amount=budget.amount, category=budget.category, user_id=current_user.id
+        name=budget.name,
+        amount=budget.amount,
+        category=budget.category,
+        user_id=current_user.id,
     )
 
     db.add(new_budget)
@@ -190,7 +227,11 @@ async def create_budgets(
 async def get_single_budget(
     budget_id: int, db=Depends(get_db), current_user=Depends(get_current_user)
 ):
-    budget = db.query(BudgetModel).filter(BudgetModel.id == budget_id, BudgetModel.user_id == current_user.id).first()
+    budget = (
+        db.query(BudgetModel)
+        .filter(BudgetModel.id == budget_id, BudgetModel.user_id == current_user.id)
+        .first()
+    )
 
     return budget
 
@@ -199,7 +240,11 @@ async def get_single_budget(
 async def delete_budget(
     budget_id: int, db=Depends(get_db), current_user=Depends(get_current_user)
 ):
-    budget = db.query(BudgetModel).filter(BudgetModel.id == budget_id, BudgetModel.user_id == current_user.id).first()
+    budget = (
+        db.query(BudgetModel)
+        .filter(BudgetModel.id == budget_id, BudgetModel.user_id == current_user.id)
+        .first()
+    )
     if budget is None:
         raise HTTPException(status_code=404, detail="Budget Not Found")
 
@@ -215,7 +260,11 @@ async def update_budget(
     db=Depends(get_db),
     current_user=Depends(get_current_user),
 ):
-    edit_budget = db.query(BudgetModel).filter(BudgetModel.id == budget_id, BudgetModel.user_id == current_user.id).first()
+    edit_budget = (
+        db.query(BudgetModel)
+        .filter(BudgetModel.id == budget_id, BudgetModel.user_id == current_user.id)
+        .first()
+    )
     if edit_budget is None:
         raise HTTPException(status_code=404, detail="Budget Not Found")
 
